c8990698ffb890b54140cfbc0b04b9de94320c74
doanmelody2607 <doanmelody2607@gmail.com>
2021-01-04 15:22:21 +0700

    homepage & about

---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ControlCarousel from "../../components/Carousel";
+import Products from "../../components/Products";
+
+const Homepage = () => {
+  return (
+    <div>
+      <ControlCarousel />
+      <br />
+      <Products />
+      <br />
+    </div>
+  );
+};
+
+export default Homepage;
